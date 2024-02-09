5ec0aa5d7aed9af293278578796c734b0034102f
Toshio Ferreira Kanegae <93012472+Toshiofk@users.noreply.github.com>
2024-02-09 11:55:14 -0300

    Add files via upload

---
+++ src - Copia/js/painel.js
@@ -0,0 +1,32 @@
+const imagens = document.querySelectorAll('.imagem-painel')
+const setaVoltar = document.getElementById('btn-voltar')
+const setaAvancar = document.getElementById('btn-avancar')
+let imagemAtual = 0
+
+function esconderImagens() {
+    imagens.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    })
+}
+
+function mostrarImagem(){    
+    imagens[imagemAtual].classList.add('mostrar')
+}
+
+setaAvancar.addEventListener('click', function () {
+    if(imagemAtual !== imagens.length - 1) {
+        imagemAtual++        
+    }
+
+    esconderImagens()
+    mostrarImagem()
+})
+
+setaVoltar.addEventListener('click', function () {
+    if (imagemAtual !== 0) {
+        imagemAtual--
+    }
+
+    esconderImagens()
+    mostrarImagem()
+})
